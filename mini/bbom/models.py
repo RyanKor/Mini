cbf26856ff516ce1b491f271969e4ccfd57e74a1
elksha <lksia12@naver.com>
2019-05-11 17:16:50 +0900

    juntae

--- mini/bbom/models.py
+++ mini/bbom/models.py
@@ -25,8 +25,6 @@ class Post(models.Model):
 
     title = models.CharField(max_length = 200, help_text = "제목을 입력하세요.")
     content = models.TextField()
-    img = models.FileField(null = None)
-    timeSet = models.TimeField(default = timezone.now)
     author = models.CharField(max_length=50, default = "")
     category = models.ForeignKey(Category, on_delete= models.CASCADE, related_name='product')
     gender = models.CharField(max_length=30, choices=CHOICES)
